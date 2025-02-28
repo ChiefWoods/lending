afe055f65ce104772e01d286b3ef2cbd65e0722b
ChiefWoods <chii.yuen@hotmail.com>
2025-02-28 11:55:30 +0800

    Remove redundant code

--- app/src/lib/solana-helpers.ts
+++ app/src/lib/solana-helpers.ts
@@ -1,65 +1,6 @@
 import { getExplorerLink, getSimulationComputeUnits } from "@solana-developers/helpers";
-import { AddressLookupTableAccount, ComputeBudgetProgram, Connection, PublicKey, Transaction, TransactionInstruction, TransactionMessage, VersionedTransaction } from "@solana/web3.js";
+import { ComputeBudgetProgram, PublicKey, TransactionInstruction, TransactionMessage, VersionedTransaction } from "@solana/web3.js";
 import { cluster, connection, LENDING_LAT } from "./constants";
-
-export async function getComputeLimitIx(
-  connection: Connection,
-  instructions: TransactionInstruction[],
-  payer: PublicKey,
-  lookupTables: Array<AddressLookupTableAccount> = []
-): Promise<TransactionInstruction | undefined> {
-  const units = await getSimulationComputeUnits(
-    connection,
-    instructions,
-    payer,
-    lookupTables
-  );
-
-  if (units) {
-    return ComputeBudgetProgram.setComputeUnitLimit({
-      units: Math.ceil(units * 1.1),
-    });
-  }
-}
-
-export async function setComputeUnitLimitAndPrice(
-  connection: Connection,
-  instructions: TransactionInstruction[],
-  payer: PublicKey,
-  lookupTables: Array<AddressLookupTableAccount> = []
-): Promise<Transaction> {
-  const tx = new Transaction();
-
-  const limitIx = await getComputeLimitIx(
-    connection,
-    instructions,
-    payer,
-    lookupTables
-  );
-
-  if (limitIx) {
-    tx.add(limitIx);
-  }
-
-  tx.add(await getComputePriceIx(connection), ...instructions);
-
-  return tx;
-}
-
-export async function getComputePriceIx(
-  connection: Connection
-): Promise<TransactionInstruction> {
-  const recentFees = await connection.getRecentPrioritizationFees();
-  const priorityFee =
-    recentFees.reduce(
-      (acc, { prioritizationFee }) => acc + prioritizationFee,
-      0
-    ) / recentFees.length;
-
-  return ComputeBudgetProgram.setComputeUnitPrice({
-    microLamports: BigInt(Math.ceil(priorityFee)),
-  });
-}
 
 export async function buildTx(ixs: TransactionInstruction[], payer: PublicKey) {
   const lat = LENDING_LAT ? [LENDING_LAT] : [];
